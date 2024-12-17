658782daac4dc07043550ff88edfe1ea88fa7885
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-17 12:46:12 +0100

    feat: add exercise 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const student2 = students.find((elm) => elm.id === 2)
+
+console.log(student2)
