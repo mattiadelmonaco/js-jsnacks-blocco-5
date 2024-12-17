d7d91113df1748b940767df31bfeda3834af5868
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-17 12:44:21 +0100

    feat: add exercise 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,10 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineOver15Cm = zucchine.filter((elm) => elm["length"] >= 15)
+console.log(zucchineOver15Cm)
+
+
+const zucchineUnder15cm = zucchine.filter((elm) => elm["length"] <= 15)
+console.log(zucchineUnder15cm)
